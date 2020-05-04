5783354683b5e57d1f09906848b9c8e13da18f1e
Sergey Dolin <v-sedoli@micorosoft.com>
2020-05-04 21:52:13 +0500

    Make First sources

---
+++ First.cxx
@@ -0,0 +1,15 @@
+#include<iostream>  
+  
+using namespace std; 
+  
+int main() 
+{ 
+	cout <<"Hello World" << endl;; 
+
+	cout << argv[0] << " Version " << First_VERSION_MAJOR << "."
+		<< First_VERSION_MINOR
+		<< endl;
+
+	return 0; 
+} 
+
